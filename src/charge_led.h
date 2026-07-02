f7eddfaa6576a58e43ef0271cb97aa7238ae4234
cumin <13809292481@163.com>
2026-07-02 13:57:35 +0800

    Stabilize LDO1 and add charge LED support

---
+++ src/charge_led.h
@@ -0,0 +1,7 @@
+#ifndef CHARGE_LED_H_
+#define CHARGE_LED_H_
+
+void charge_led_init(void);
+void charge_led_poll(void);
+
+#endif /* CHARGE_LED_H_ */
--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
 #include <hal/nrf_reset.h>
 
 #include "at_handler.h"
+#include "charge_led.h"
 #include "factory_storage.h"
 
 #define UART_NODE DT_NODELABEL(uart21)
@@ -376,6 +377,7 @@ int main(void)
 
 	at_handler_init(uart_dev, regulator_parent, &g_persist);
 	at_handler_early_init();
+	charge_led_init();
 
 	uart_send_line("=== XIAO nRF54LM20A Factory UART V3 ===");
 	uart_send_line("UART: UART21 @ 115200");
@@ -404,6 +406,7 @@ int main(void)
 		}
 
 		at_handler_poll_background();
+		charge_led_poll();
 
 		if (!handled_uart) {
 			k_usleep(100);
